475bc312a703f3c9581f32423135859794c65372
aditya <adisingh925@gmail.com>
2024-09-26 00:33:22 +0530

    more slower animation, more precise location at the cost of battery, handled disposing of socket, reformatted code

--- app/src/main/java/com/app/android/assistant/presentation/MainActivity.kt
+++ app/src/main/java/com/app/android/assistant/presentation/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.app.android.assistant.presentation
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
@@ -45,10 +47,12 @@ import androidx.compose.material.icons.filled.Mic
 import androidx.compose.material.icons.filled.Sensors
 import androidx.compose.material.icons.filled.Sync
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -59,20 +63,31 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.PathBuilder
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.times
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.wear.ambient.AmbientLifecycleObserver
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.ButtonDefaults
 import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.Text
+import com.app.android.assistant.presentation.MainActivity.Companion.ELEVEN_LABS_API_KEY
+import com.app.android.assistant.presentation.MainActivity.Companion.ELEVEN_LABS_BASE_URL
+import com.app.android.assistant.presentation.MainActivity.Companion.ELEVEN_LABS_VOICE_ID
+import com.app.android.assistant.presentation.MainActivity.Companion.SOCKET_SERVER_CONNECTION_TOKEN
+import com.app.android.assistant.presentation.MainActivity.Companion.SOCKET_URL
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationTokenSource
 import com.google.android.gms.tasks.Task
 import io.socket.client.IO
 import io.socket.client.Socket
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
@@ -83,23 +98,45 @@ import okhttp3.Response
 import org.json.JSONObject
 import java.io.File
 import java.io.IOException
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
-
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-
-    companion object {
-        const val LOCATION_PERMISSION_CODE = 201
-    }
 
     private var shouldShowPermissionScreen =
         false // Track if we need to show manual permission screen
 
+    companion object {
+        const val LOCATION_PERMISSION_CODE = 201
+        const val SOCKET_URL = "wss://ws.dreadtools.com"
+        const val SOCKET_SERVER_CONNECTION_TOKEN = "abc123"
+        const val ELEVEN_LABS_API_KEY = "sk_62b236b81ca03a60812a8c1e92e6f28c985c7daa665fc10e"
+        const val ELEVEN_LABS_VOICE_ID = "9BWtsMINqrJLrRacOk9x"
+        const val ELEVEN_LABS_BASE_URL = "https://api.elevenlabs.io"
+    }
+
+    private val ambientCallback = object : AmbientLifecycleObserver.AmbientLifecycleCallback {
+        override fun onEnterAmbient(ambientDetails: AmbientLifecycleObserver.AmbientDetails) {
+            Log.d("Ambient", "Entering ambient mode")
+        }
+
+        override fun onExitAmbient() {
+            Log.d("Ambient", "Exiting ambient mode")
+        }
+
+        override fun onUpdateAmbient() {
+            // ... Called by the system in order to allow the app to periodically
+            // update the display while in ambient mode. Typically the system will
+            // call this every 60 seconds.
+            Log.d("Ambient", "Updating ambient mode")
+        }
+    }
+
+    private val ambientObserver = AmbientLifecycleObserver(this, ambientCallback)
+
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        lifecycle.addObserver(ambientObserver)
 
         setContent {
             if (shouldShowPermissionScreen) {
@@ -149,6 +186,7 @@ class MainActivity : ComponentActivity() {
         } else {
             // If any permission is denied, show the manual permission screen
             shouldShowPermissionScreen = true
+
             setContent {
                 PermissionScreen(
                     requestPermissions = ::requestPermissionsManually
@@ -160,57 +198,11 @@ class MainActivity : ComponentActivity() {
 
     // Called when both permissions are granted
     private fun onPermissionsGranted() {
-        getLastKnownLocation()
-
         setContent {
             WearApp(
                 checkLocationPermission = ::checkLocationPermission,
-                fetchUserLocation()
+                this
             )
-        }
-    }
-
-    private fun getLastKnownLocation() {
-        try {
-            if (checkLocationPermission()) {
-                val locationResult: Task<Location> = fusedLocationClient.lastLocation
-                locationResult.addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        task.result?.let { location ->
-                            Log.d(
-                                "Location Info",
-                                "Latitude: ${location.latitude}, Longitude: ${location.longitude}"
-                            )
-                            storeUserLocation(location.latitude, location.longitude)
-                        }
-                    } else {
-                        Log.d("Location Info", "Location not found")
-                    }
-                }
-            }
-        } catch (e: SecurityException) {
-            e.printStackTrace()
-        }
-    }
-
-    private fun storeUserLocation(latitude: Double, longitude: Double) {
-        val sharedPreferences = getSharedPreferences("UserLocation", MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.putString("Latitude", latitude.toString())
-        editor.putString("Longitude", longitude.toString())
-        editor.apply()
-    }
-
-    private fun fetchUserLocation(): Pair<Double, Double>? {
-        val sharedPreferences = getSharedPreferences("UserLocation", MODE_PRIVATE)
-
-        val latitude = sharedPreferences.getString("Latitude", null)
-        val longitude = sharedPreferences.getString("Longitude", null)
-
-        return if (latitude != null && longitude != null) {
-            Pair(latitude.toDouble(), longitude.toDouble())
-        } else {
-            null // Handle the case where location is not stored
         }
     }
 
@@ -259,25 +251,31 @@ fun PermissionScreen(
     }
 }
 
+@SuppressLint("MissingPermission")
 @Composable
 fun WearApp(
     checkLocationPermission: () -> Boolean,
-    location: Pair<Double, Double>? = null
+    context: Context
 ) {
-    Log.d("Location", "User location: $location")
-
+    val scope = rememberCoroutineScope()
     var buttonState by remember { mutableStateOf(ButtonState.Connecting) }
     var textForVoiceInput by remember { mutableStateOf("") }
     val receivedMessage: StringBuilder by remember { mutableStateOf(StringBuilder()) }
     var bitmapImage by remember { mutableStateOf<Bitmap?>(null) } // State to hold the bitmap image
+    val locationClient = remember {
+        LocationServices.getFusedLocationProviderClient(context)
+    }
+    var latitudes by remember { mutableStateOf("") }
+    var longitudes by remember { mutableStateOf("") }
+    var expectedDataType by remember { mutableStateOf("") }
 
     val socket = remember {
         try {
             val options = IO.Options().apply {
-                auth = mapOf("token" to "abc123")
+                auth = mapOf("token" to SOCKET_SERVER_CONNECTION_TOKEN)
             }
-            Log.d("SocketIO", "Creating the Socket instance")
-            IO.socket("wss://ws.dreadtools.com", options)
+            Log.d("SocketIO", "Connecting to Socket Server...")
+            IO.socket(SOCKET_URL, options)
         } catch (e: Exception) {
             e.printStackTrace()
             null
@@ -286,7 +284,7 @@ fun WearApp(
 
     val voiceIntent: Intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
         putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-        putExtra(RecognizerIntent.EXTRA_PROMPT, "test")
+        putExtra(RecognizerIntent.EXTRA_PROMPT, "Please speak now")
     }
 
     val voiceLauncher = rememberLauncherForActivityResult(
@@ -303,28 +301,69 @@ fun WearApp(
         }
     }
 
+    /**
+     * Fetch the user's location in separate thread
+     */
     LaunchedEffect(Unit) {
-        // Handle socket events
+        scope.launch(Dispatchers.IO) {
+            if (checkLocationPermission()) {
+                /** Fetch accurate location data */
+                val priority = Priority.PRIORITY_HIGH_ACCURACY
+                locationClient.getCurrentLocation(
+                    priority,
+                    CancellationTokenSource().token,
+                ).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val fetchedLocation = it.result
+                        latitudes = fetchedLocation.latitude.toString()
+                        longitudes = fetchedLocation.longitude.toString()
+                        Log.d(
+                            "Location",
+                            "Fetched location: ${fetchedLocation.latitude}, ${fetchedLocation.longitude}"
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    DisposableEffect(Unit) {
+        onDispose {
+            Log.d("SocketIO", "Disconnecting from the server")
+            socket?.disconnect()
+        }
+    }
+
+    /**
+     * Connect to the Socket server and handle events
+     */
+    LaunchedEffect(Unit) {
         socket?.on(Socket.EVENT_CONNECT) {
             Log.d("SocketIO", "Connected to the server")
             voiceLauncher.launch(voiceIntent)
             buttonState = ButtonState.Recording
+
+            /** Register Client */
             socket.emit("register", "client")
         }?.on(Socket.EVENT_DISCONNECT) {
             buttonState = ButtonState.Connecting
         }?.on("chat_stream") {
             val message = it[0] as String
             if (message.isNotEmpty()) {
-                Log.d("SocketIO", "Received message: $message")
+                Log.d("SocketIO", "Received message : $message")
                 if (message != "-SOM-" && message != "-EOM-") {
                     receivedMessage.append(message)
                 } else if (message == "-EOM-") {
                     streamTextToSpeech(
-                        "sk_62b236b81ca03a60812a8c1e92e6f28c985c7daa665fc10e",
-                        receivedMessage.toString(), "9BWtsMINqrJLrRacOk9x"
+                        receivedMessage.toString()
                     ) { byteArray ->
                         playAudio(byteArray) {
-                            buttonState = ButtonState.Connected
+                            if (expectedDataType == "text") {
+                                if (checkLocationPermission()) {
+                                    voiceLauncher.launch(voiceIntent)
+                                    buttonState = ButtonState.Recording
+                                }
+                            }
                         }
                     }
 
@@ -333,7 +372,7 @@ fun WearApp(
             }
         }?.on("chat_image") {
             val message = it[0] as String
-            Log.d("SocketIO", "Received image: $message")
+            Log.d("SocketIO", "Received image : $message")
 
             if (isBase64Image(message)) {
                 Log.d("SocketIO", "Image is base64-encoded.")
@@ -341,9 +380,10 @@ fun WearApp(
                 // Extract the base64 part
                 val base64Data = message.substringAfter(",")
 
-                // Decode the base64 data
-                bitmapImage = decodeBase64ToBitmap(base64Data) // Update the bitmap state
+                // Decode the base64 data and update the bitmap state
+                bitmapImage = decodeBase64ToBitmap(base64Data)
 
+                // change the state to image
                 buttonState = ButtonState.Image
             } else {
                 Log.d("SocketIO", "Image is not base64-encoded.")
@@ -355,11 +395,18 @@ fun WearApp(
     }
 
     LaunchedEffect(textForVoiceInput) {
-        Log.d("VoiceInput", "Received text: $textForVoiceInput")
+        Log.d("VoiceInput", "Received text from User : $textForVoiceInput")
+
+        expectedDataType = if (textForVoiceInput.contains("image", ignoreCase = true)) {
+            "image"
+        } else {
+            "text"
+        }
+
         if (textForVoiceInput.isNotEmpty()) {
             socket?.emit(
                 "chat_message",
-                textForVoiceInput,
+                textForVoiceInput.lowercase(Locale.ENGLISH),
                 null,
                 2,
                 110,
@@ -368,8 +415,10 @@ fun WearApp(
                 false,
                 "USER",
                 "CONVERSATIONID",
-                "${location?.first ?: "Unknown"}, ${location?.second ?: "Unknown"}"
+                "${latitudes},${longitudes}"
             )
+
+            // change the state to processing
             buttonState = ButtonState.Processing
         }
     }
@@ -401,7 +450,10 @@ fun WearApp(
                     ButtonState.Processing -> Unit
 
                     ButtonState.Image -> {
-                        buttonState = ButtonState.Connected
+                        if (checkLocationPermission()) {
+                            voiceLauncher.launch(voiceIntent)
+                            buttonState = ButtonState.Recording
+                        }
                         bitmapImage = null // Clear the image
                     }
                 }
@@ -430,20 +482,27 @@ fun CircularStateButton(
         animationSpec = if (currentState == ButtonState.Connected) {
             infiniteRepeatable(
                 animation = keyframes {
-                    durationMillis = 3000 // Slower, for a "breathing" effect
+                    durationMillis =
+                        5750 // Total duration for the entire pulse cycle (1.5s + 0.75s + 2.5s + 1s)
+
+                    // Enlarge phase (1.5 seconds)
                     defaultButtonSize at 0 using LinearOutSlowInEasing
-                    defaultButtonSize * 1.2f at 1500 using LinearOutSlowInEasing // Expanding phase
-                    defaultButtonSize at 3000 using LinearOutSlowInEasing // Contracting phase
+                    defaultButtonSize * 1.2f at 1500 using LinearOutSlowInEasing
+
+                    // Pause after enlarging (0.75 seconds)
+                    defaultButtonSize * 1.2f at 2250
+
+                    // Decrease phase (2.5 seconds)
+                    defaultButtonSize at 4750 using LinearOutSlowInEasing
+
+                    // Pause after contracting (1 second)
+                    defaultButtonSize at 5750
                 },
-                repeatMode = RepeatMode.Reverse
+                repeatMode = RepeatMode.Restart
             )
         } else {
-            tween(
-                durationMillis = 300, // Short animation for other states
-                easing = FastOutSlowInEasing // Apply easing
-            )
-        },
-        label = ""
+            tween(durationMillis = 0) // No animation for other states
+        }, label = ""
     )
 
     // Define rotation animation for the icon in Processing state
@@ -531,9 +590,7 @@ fun decodeBase64ToBitmap(base64Str: String): Bitmap? {
 }
 
 fun streamTextToSpeech(
-    apiKey: String,
     text: String,
-    voiceId: String,
     onStreamReady: (byteArray: ByteArray?) -> Unit
 ) {
     val client = OkHttpClient()
@@ -548,8 +605,8 @@ fun streamTextToSpeech(
     jsonBody.put("voice_settings", voiceSettings)
 
     val request = Request.Builder()
-        .url("https://api.elevenlabs.io/v1/text-to-speech/${voiceId}/stream")
-        .addHeader("xi-api-key", apiKey)
+        .url("${ELEVEN_LABS_BASE_URL}/v1/text-to-speech/${ELEVEN_LABS_VOICE_ID}/stream")
+        .addHeader("xi-api-key", ELEVEN_LABS_API_KEY)
         .addHeader("content-type", "application/json")
         .post(RequestBody.create("application/json".toMediaTypeOrNull(), jsonBody.toString()))
         .build()
@@ -595,11 +652,12 @@ fun playAudio(byteArray: ByteArray?, onCompletion: () -> Unit) {
                 player.start()
             }
             mediaPlayer.setOnCompletionListener {
-                onCompletion() // Call the completion handler
                 mediaPlayer.release() // Release MediaPlayer resources
+                onCompletion()
             }
         } catch (e: Exception) {
             Log.e("MediaPlayerError", "Error playing audio: ${e.localizedMessage}")
+            onCompletion()
         }
     } ?: run {
         Log.e("TTS", "Audio byte array is null, cannot play audio.")
